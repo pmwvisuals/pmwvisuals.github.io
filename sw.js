09bb05301bf9ab2d49bbea71cb08eda1523c7946
pmwvisuals <manvirupemitha@gmail.com>
2026-06-28 17:37:26 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11211814
+    "zoneId": 11211838
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
